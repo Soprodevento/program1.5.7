05ae640af0bea363c58e1fa6c4a3faa534f47af5
Soprodevento <Sashinka2803@gmail.com>
2024-03-16 14:14:59 +0300

    исправил

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,11 @@
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] firstArray = {1, 3, 7, 5};
-        int[] secondArray = {8, 4, 2, 4};
+        int[] secondArray = {8, 4, 2, 22};
         int[] mergedArray = mergeAndSort(firstArray, secondArray);
         bubbleSort(mergedArray);
-
-        System.out.print("[");
-        for (int i = 0; i < mergedArray.length; i++) {
-            if (i > 0) {
-                System.out.print(", ");
-            }
-            System.out.print(mergedArray[i]);
-        }
-        System.out.println("]");
+        System.out.print(Arrays.toString(mergedArray));
     }
 
     public static int[] mergeAndSort(int[] firstArray, int[] secondArray) {
